9fe7b28e9652e38f6a850a74b341270b78b78db6
CyberGrim <cybergrim99@gmail.com>
2025-10-21 01:14:35 -0400

    Even more work on Boot.dev's Static Site Generator

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from textnode import TextNode, TextType
+from textnode import TextNode, TextType, text_node_to_html_node
 
 
 class TestTextNode(unittest.TestCase):
@@ -18,6 +18,55 @@ class TestTextNode(unittest.TestCase):
         node = TextNode("This is a text node", TextType.BOLD, None)
         node2 = TextNode("This is a text node", TextType.BOLD)
         self.assertEqual(node, node2)
+
+
+class TestTextNodeToHtmlNode(unittest.TestCase):
+    def test_text_conversion(self):
+        node = TextNode("Hello, world!", TextType.TEXT)
+        html_node = text_node_to_html_node(node)
+        self.assertIsNone(html_node.tag)
+        self.assertEqual(html_node.value, "Hello, world!")
+        self.assertEqual(html_node.props, {})
+
+    def test_bold_conversion(self):
+        node = TextNode("Bold text", TextType.BOLD)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "b")
+        self.assertEqual(html_node.value, "Bold text")
+        self.assertEqual(html_node.props, {})
+
+    def test_italic_conversion(self):
+        node = TextNode("Italic text", TextType.ITALIC)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "i")
+        self.assertEqual(html_node.value, "Italic text")
+        self.assertEqual(html_node.props, {})
+
+    def test_code_conversion(self):
+        node = TextNode("def hello(): pass", TextType.CODE)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "code")
+        self.assertEqual(html_node.value, "def hello(): pass")
+        self.assertEqual(html_node.props, {})
+
+    def test_link_conversion(self):
+        node = TextNode("Click me", TextType.LINK, "https://www.example.com")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "a")
+        self.assertEqual(html_node.value, "Click me")
+        self.assertEqual(html_node.props, {"href": "https://www.example.com"})
+
+    def test_image_conversion(self):
+        node = TextNode("Alt text", TextType.IMAGE, "image.png")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "img")
+        self.assertEqual(html_node.value, "")
+        self.assertEqual(html_node.props, {"src": "image.png", "alt": "Alt text"})
+
+    def test_invalid_type_conversion(self):
+        node = TextNode("Invalid", "invalid_type")
+        with self.assertRaises(Exception):
+            text_node_to_html_node(node)
 
 
 if __name__ == "__main__":
